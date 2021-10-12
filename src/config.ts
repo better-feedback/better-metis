17a47d857f984942e73925803a276f63c8141ec0
Dong-Ha Kim <dongha.kim210@gmail.com>
2021-10-12 18:51:25 +0200

    Add layout component

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+  projectName: "Better",
+  siteTitle: "Better App",
+};
+
+export default config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Layout from "../components/layout";
+
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <Layout title="Home">
+      <div>Home</div>
+    </Layout>
+  );
+};
+
+export default Home;
