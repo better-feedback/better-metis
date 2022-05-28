fa6065c5fd1b121138dc35f2c4d5a9f37e59eded
Aimen Sahnoun <aimen.sahnoun@lyntlabs.com>
2022-05-28 06:08:15 +0300

    added prices in dollar

--- src/features/issues/components/issue-details-sidebar.tsx
+++ src/features/issues/components/issue-details-sidebar.tsx
@@ -15,6 +15,8 @@ export default function IssueDetailsSidebar(props: { issue: Issue }) {
   const walletIsSignedInQuery = useWalletIsSignedInQuery();
 
   const [bounty, setBounty] = useState(null);
+  const [pool, setPool] = useState("");
+  const [poolInDollars, setPoolInDollars] = useState<string>("");
 
   /* A hook that is called when the component is mounted. 
   In order to fetch the bounty stored in the contract
@@ -24,11 +26,33 @@ export default function IssueDetailsSidebar(props: { issue: Issue }) {
     viewFunction("getBountyByIssue", { issueId: props.issue.number })
       .then((res) => {
         setBounty(res);
+        setPool(utils.format.formatNearAmount(res?.pool));
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
+
+  
+
+  
+  useEffect(() => {
+    /* This is a function that is called when a bounty is found. It fetches the current price of
+    NEAR from the CoinGecko API and then calculates the value of the bounty pool in USD. */
+    (async () => {
+      if (!bounty) return;
+      const apiData = await fetch(
+        "https://api.coingecko.com/api/v3/coins/near"
+      );
+      const nearData = await apiData.json();
+
+      setPoolInDollars(
+        (nearData?.market_data?.current_price?.usd * parseFloat(pool)).toFixed(
+          2
+        )
+      );
+    })();
+  }, [bounty , pool]);
 
   return (
     <aside>
@@ -39,7 +63,8 @@ export default function IssueDetailsSidebar(props: { issue: Issue }) {
           <div>
             {!bounty
               ? "-"
-              : utils.format.formatNearAmount(bounty.pool) + " Near"}
+              : utils.format.formatNearAmount(bounty.pool) + " Near"}{" "}
+            - ${poolInDollars}
           </div>
         }
       />
