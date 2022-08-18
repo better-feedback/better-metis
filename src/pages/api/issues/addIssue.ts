9bb80e2fd65041783eeba4f64d526d6d01c95344
Aimen Sahoun <info@aimensahnoun.com>
2022-08-18 20:20:07 +0300

    Add ability to create Github issue from API

--- src/pages/api/issues/addIssue.ts
+++ src/pages/api/issues/addIssue.ts
@@ -5,20 +5,35 @@ import {
 
 import type { NextApiRequest, NextApiResponse } from "next";
 
+import { getUserAccessKey } from "../../../features/api-routes/api/helpers/authProfile";
 
 import { Octokit } from "octokit";
-import axios from "axios";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
+    const { userId, title, body, isGithubAuth } = req.body;
+
+    const accessToken = (isGithubAuth
+      ? await getUserAccessKey(userId)
+      : process.env.GITHUB_PAT) as any;
+
+    const octokit = new Octokit({
+      auth: !isGithubAuth ? accessToken : accessToken?.identities[0].access_token ,
+    });
+
     
-    const {userId , title, body, isGithubAuth} = req.body;
 
-    // const accessToken = isGithubAuth ? await getAccessToken(req, res) : null;
+    const newIssue = await octokit.rest.issues.create({
+      owner: process.env.NEXT_PUBLIC_REPO_OWNER as string,
+      repo: process.env.NEXT_PUBLIC_REPO_NAME as string,
+      title,
+      body : isGithubAuth ? body : `**Created By: ${userId}**\n ${body}`,
+    });
 
+    res.status(200).json({ message: newIssue.data });
   } catch (error) {
     apiErrorHandler(res, error);
   }
